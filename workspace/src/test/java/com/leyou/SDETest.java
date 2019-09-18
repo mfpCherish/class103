2f0df202ae5a287ada58ba73b59547d6aa10c099
mfp <mfp_cherish@126.com>
2019-09-18 20:37:58 +0800

    修改并添加了fun02

--- workspace/src/test/java/com/leyou/SDETest.java
+++ workspace/src/test/java/com/leyou/SDETest.java
@@ -42,5 +42,10 @@ public class SDETest{
         personList.forEach(person ->
                 System.out.println(person));
     }
+	
+	@Test
+	public void fun02(){
+        System.out.println("hello 02"));
+    }
 
 }
